a1d5767e6c2f974aa25ef42670ad9dd1bfa95ba4
GuiVianaDev <guilhermevianna@gmail.com>
2025-08-22 21:22:10 -0300

    chore: config tanstack react query

--- frontend-zeine/src/App.tsx
+++ frontend-zeine/src/App.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RegisterUser } from './pages/register-user';
 import { SignIn } from './pages/signIn-User';
 
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<SignIn />} index />
-        <Route element={<RegisterUser />} path={'/register'} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<SignIn />} index />
+          <Route element={<RegisterUser />} path={'/register'} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
