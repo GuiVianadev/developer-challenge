02c7c45024b5c71ed3f11d44329d25c82107967e
GuiVianaDev <guilhermevianna@gmail.com>
2025-08-22 22:08:02 -0300

    chore: config CORSmiddleware

--- backend-zeine/app/main.py
+++ backend-zeine/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.auth import router as auth_router
 from routes.user import router as user_router
 from routes.contacts import router as contacts_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # React local
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         
+    allow_credentials=True,
+    allow_methods=["*"],           
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(auth_router)
